f8b4a883ac589526b7a7daa8d46e4e345cdfd526
adhirajchhoda <noreply@github.com>
2025-08-25 13:03:21 -0400

    half of Lab01

--- app/src/main/java/com/adhiraj/lab01/MainActivity.java
+++ app/src/main/java/com/adhiraj/lab01/MainActivity.java
@@ -1,24 +1,17 @@
 package com.adhiraj.lab01;
 
 import android.os.Bundle;
-
-import androidx.activity.EdgeToEdge;
+import android.widget.TextView;
 import androidx.appcompat.app.AppCompatActivity;
-import androidx.core.graphics.Insets;
-import androidx.core.view.ViewCompat;
-import androidx.core.view.WindowInsetsCompat;
+import androidx.core.content.ContextCompat;
 
 public class MainActivity extends AppCompatActivity {
-
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        EdgeToEdge.enable(this);
         setContentView(R.layout.activity_main);
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
-            Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
-            return insets;
-        });
+
+        TextView textView = findViewById(R.id.textView);
+        textView.setTextColor(ContextCompat.getColor(this, R.color.nonchalant_red));
     }
 }
