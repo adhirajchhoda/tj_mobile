c2fb2a5ad1d3e7dfe82aa5f847e3f3e4f4395173
adhirajchhoda <noreply@github.com>
2025-08-25 14:09:26 -0400

    updated Lab01 logic with coloring based on count

--- app/src/main/java/com/adhiraj/lab01/MainActivity.java
+++ app/src/main/java/com/adhiraj/lab01/MainActivity.java
@@ -5,30 +5,41 @@ import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.TextView;
+
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.core.content.ContextCompat;
 
 public class MainActivity extends AppCompatActivity {
     Button incrementButton;
     TextView greetingDisplay;
+    TextView countDisplay;
     int count = 0;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         incrementButton = findViewById(R.id.increment_button);
+        countDisplay = findViewById(R.id.count_textview);
         greetingDisplay = findViewById(R.id.greeting_textview);
         incrementButton.setOnClickListener(v -> {
-            System.out.println("incrementing" + ++count);
             Log.i("testing: incrementing", String.valueOf(++count));
-            greetingDisplay.setText(String.valueOf(count));
+            countDisplay.setText(String.valueOf(count));
+            updateCountDisplayColor();
         });
-
-
-        TextView textView = findViewById(R.id.textView);
-        textView.setTextColor(ContextCompat.getColor(this, R.color.nonchalant_red));
     }
+
     public void decrement(View view) {
         Log.i("testing: decrementing", String.valueOf(--count));
+        updateCountDisplayColor();
+        countDisplay.setText(String.valueOf(count));
+    }
+
+    public void updateCountDisplayColor() {
+        if (count < 0) {
+            countDisplay.setTextColor(ContextCompat.getColor(this, R.color.nonchalant_red));
+        } else {
+            countDisplay.setTextColor(ContextCompat.getColor(this, R.color.black));
+        }
     }
 }
